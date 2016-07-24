4b8b38fe973afcde3475e7e5cb02d68fadd91db4
Michael Staal-Olsen <m@mstaal.com>
2016-07-24 21:41:45 +0200

    Make restructuring

--- coordinates.js
+++ coordinates.js
@@ -1,16 +1,16 @@
 function showError(error) {
     switch(error.code) {
         case error.PERMISSION_DENIED:
-            loc.innerHTML = "User denied the request for Geolocation."
+            loc.value = "User denied the request for Geolocation."
             break;
         case error.POSITION_UNAVAILABLE:
-            loc.innerHTML = "Location information is unavailable."
+            loc.value = "Location information is unavailable."
             break;
         case error.TIMEOUT:
-            loc.innerHTML = "The request to get user location timed out."
+            loc.value = "The request to get user location timed out."
             break;
         case error.UNKNOWN_ERROR:
-            loc.innerHTML = "An unknown error occurred."
+            loc.value = "An unknown error occurred."
             break;
     }
 }
