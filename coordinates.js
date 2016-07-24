bf9571d2cecc47ba5329bc9163894b5ef96819e8
Michael Staal-Olsen <m@mstaal.com>
2016-07-24 21:51:01 +0200

    Add Google Map

--- coordinates.js
+++ coordinates.js
@@ -1,5 +1,6 @@
 var loc = document.getElementById("location");
 
+// Geolocation
 function getLocation() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(showPosition, showError);
@@ -29,3 +30,34 @@ function showError(error) {
             break;
     }
 }
+
+// Google Maps
+var myPos = new google.maps.LatLng(41.890205,12.492245)
+function initializeMap() {
+  var mapProp = {
+    center: myPos,
+    zoom:18,
+    panControl:true,
+    zoomControl:true,
+    mapTypeControl:false,
+    scaleControl:true,
+    streetViewControl:false,
+    overviewMapControl:true,
+    rotateControl:false,
+    mapTypeId: google.maps.MapTypeId.HYBRID
+  };
+  var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+  map.setTilt(0);
+  var marker = new google.maps.Marker({
+    position:myPos
+  });
+  marker.setMap(map);
+  google.maps.event.addListener(map, 'center_changed', function() {
+  // 3 seconds after the center of the map has changed, pan back to the
+  // marker.
+    window.setTimeout(function() {
+      marker.setPosition(map.getCenter());
+    }, 1);
+  });
+}
+google.maps.event.addDomListener(window, 'load', initialize);
