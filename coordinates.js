4c3f702462d3ceb7358214c93881d9446989b436
Michael Staal-Olsen <m@mstaal.com>
2018-11-11 16:03:46 +0100

    Update coordinates.js

--- coordinates.js
+++ coordinates.js
@@ -21,7 +21,7 @@ function setLocationVariables(latitude, longitude) {
     var locactionlinkValue = "google.com/maps/?q=" + latitude + "," + longitude; // Google Maps link
     var smsbody = "sms:&body=";
     var smslinkHref = smsbody + locactionlinkValue;
-    locationlinkText.innerHTML = "HEJ";
+    locationlinkText.innerHTML = locactionlinkValue;
     [locationlink.value, smslink.href] = [locactionlinkValue, smslinkHref];
 }
 
